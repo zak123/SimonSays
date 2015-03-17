815b4d06cff699fba322c3ed7b4a17b81428ac22
zak123 <zacharym123@gmail.com>
2015-03-17 13:49:08 -0700

    Initial Commit

---
+++ SimonSays/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimonSays
+//
+//  Created by Zachary Mallicoat on 3/17/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimonSays/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimonSays
+//
+//  Created by Zachary Mallicoat on 3/17/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
